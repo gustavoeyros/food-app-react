939aa3aa6a740b290234c144ea53d4708ff6151f
Gustavo Eyros <gustavoeyros@hotmail.com>
2023-01-09 14:36:43 -0300

    feat: sending data to firebase

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -24,6 +24,16 @@ const Cart = (props) => {
     setIsCheckout(true);
   };
 
+  const submitOrderHandler = (userData) => {
+    fetch(import.meta.env.VITE_FIREBASE_ORDERS, {
+      method: "POST",
+      body: JSON.stringify({
+        user: userData,
+        orderedItems: cartCtx.items,
+      }),
+    });
+  };
+
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {cartCtx.items.map((item) => (
@@ -59,7 +69,9 @@ const Cart = (props) => {
         <span>Total</span>
         <span>{totalAmount}</span>
       </div>
-      {isCheckout && <Checkout onCancel={props.onClose} />}
+      {isCheckout && (
+        <Checkout onConfirm={submitOrderHandler} onCancel={props.onClose} />
+      )}
       {!isCheckout && modalActions}
     </Modal>
   );
